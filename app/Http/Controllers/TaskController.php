fc244a452a5adee45b0dad478690e4608f76a0ab
ChernyakovSI <paladin_cool@inbox.ru>
2017-10-08 20:12:07 +0300

    html version of TASK type of task is only idea's generation

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use Validator;
+class CabinetController extends Controller
+{
+    public function index(Request $request,$arguments) {
+
+        $data = [];
+        return view('task.task',$data);
+    }
+
+}
